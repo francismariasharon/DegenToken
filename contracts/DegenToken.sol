2f8059ca51c8a62497a791b05b46d0e6cc73f381
francismariasharon <149961925+francismariasharon@users.noreply.github.com>
2023-12-06 16:05:35 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DegenTok is ERC20, ERC20Burnable, Ownable {
+contract DegenToken is ERC20, ERC20Burnable, Ownable {
     
     string  minimum_balance;
     string public degen_shop;
